9e52fd7d8db9f1e5d90f7455c82e2123cf548ce5
sabiransari <sabir.ansari@idrive.com>
2025-05-13 14:59:26 +0530

    controller folder

---
+++ Backend/src/middlewares/authMiddleware.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+exports.protect = (req, res, next) => {
+  const token = req.header('Authorization');
+  if (!token) return res.status(401).json({ error: 'Access denied' });
+
+  try {
+    const decoded = jwt.verify(token.replace('Bearer ', ''), process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(400).json({ error: 'Invalid token' });
+  }
+};
